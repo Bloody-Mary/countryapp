950031802d27d0c9a86f68ddcffe2b9017baa9a4
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-16 23:46:15 +0300

    Updated class Main

--- src/main/java/ru/babushkina/countryapp/_main/Main.java
+++ src/main/java/ru/babushkina/countryapp/_main/Main.java
@@ -13,39 +13,44 @@ public class Main {
 
         while (true) {
             System.out.println("Меню:");
-            System.out.println("1. Вывести столицу");
-            System.out.println("2. Вывести количество областей");
-            System.out.println("3. Вывести площадь государства");
-            System.out.println("4. Вывести областные центры");
-            System.out.println("5. Вывести средний возраст жителей");
-            System.out.println("6. Вывести жителей с именем, состоящим из N букв");
-            System.out.println("7. Вывести список жителей, у которых имя начинается с буквы m");
-            System.out.println("8. Выйти из программы");
+            System.out.println("1. Вывести информацию о государстве");
+            System.out.println("2. Вывести столицу");
+            System.out.println("3. Вывести количество областей");
+            System.out.println("4. Вывести площадь государства");
+            System.out.println("5. Вывести областные центры");
+            System.out.println("6. Вывести средний возраст жителей");
+            System.out.println("7. Вывести жителей с именем, состоящим из N букв");
+            System.out.println("8. Вывести список жителей, у которых имя начинается с буквы m");
+            System.out.println("9. Выйти из программы");
             System.out.println("Введите номер команды: ");
 
             int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Столица: " + country.getCapital().getName());
+                    System.out.println(country.toString());
                     break;
                 case 2:
+                    System.out.println("Столица: " + country.getCapital().getName());
+                    break;
+
+                case 3:
                     System.out.println("Количество областей: " + country.getRegions().size());
                     break;
-                case 3:
+                case 4:
                     double totalArea = 0;
                     for (Region region : country.getRegions()) {
                         totalArea += region.getArea();
                     }
                     System.out.println("Площадь государства: " + totalArea);
                     break;
-                case 4:
+                case 5:
                     System.out.println("Областные центры:");
                     for (Region region : country.getRegions()) {
                         System.out.println("Область: " + region.getName() + ", Областной центр: " + region.getCenter());
                     }
                     break;
-                case 5:
+                case 6:
                     int totalAge = 0;
                     int citizenCount = country.getCitizens().size();
                     for (Citizen citizen : country.getCitizens()) {
@@ -54,7 +59,7 @@ public class Main {
                     double averageAge = totalAge / (double) citizenCount;
                     System.out.println("Средний возраст жителей: " + averageAge);
                     break;
-                case 6:
+                case 7:
                     System.out.println("Введите количество букв в имени:");
                     int letterCount = scanner.nextInt();
                     System.out.println("Жители с именем, состоящим из " + letterCount + " букв:");
@@ -64,12 +69,12 @@ public class Main {
                         }
                     }
                     break;
-                case 7:
+                case 8:
                     System.out.println("Введите первую букву:");
                     char startingLetter = scanner.next().charAt(0);
                     country.printCitizensWithLetter(startingLetter);
                     break;
-                case 0:
+                case 9:
                     System.exit(0);
                     break;
                 default:
