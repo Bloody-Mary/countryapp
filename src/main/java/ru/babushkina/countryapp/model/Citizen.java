9b645d05ef46bedf105268df2e450332641dbd31
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-16 21:45:40 +0300

    Fixed class Citizen

---
+++ src/main/java/ru/babushkina/countryapp/model/Citizen.java
@@ -0,0 +1,36 @@
+package ru.babushkina.countryapp.model;
+
+public class Citizen {
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final Country country;
+
+    public Citizen(int id, String firstName, String lastName, int age, Country country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public String toString() {
+        return "Гражданин " + id + ": " + firstName + " " + lastName + ", Возраст: " + age;
+    }
+
+
+}
+
