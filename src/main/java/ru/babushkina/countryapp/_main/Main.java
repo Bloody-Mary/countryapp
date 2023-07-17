19ac9d3074553f39aa521cc4700b49684c401b1c
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-17 15:04:22 +0300

    Updated and moved class Main to _main package

--- src/main/java/ru/babushkina/countryapp/_main/Main.java
+++ src/main/java/ru/babushkina/countryapp/_main/Main.java
@@ -1,85 +1,14 @@
 package ru.babushkina.countryapp._main;
 
-import ru.babushkina.countryapp.model.Citizen;
-import ru.babushkina.countryapp.model.Country;
-import ru.babushkina.countryapp.model.Region;
-
-import java.util.Scanner;
+import ru.babushkina.countryapp.model.entity.Country;
+import ru.babushkina.countryapp.controller.MainController;
+import ru.babushkina.countryapp.view.MainView;
 
 public class Main {
     public static void main(String[] args) {
         Country country = Country.getInstance();
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Меню:");
-            System.out.println("1. Вывести информацию о государстве");
-            System.out.println("2. Вывести столицу");
-            System.out.println("3. Вывести количество областей");
-            System.out.println("4. Вывести площадь государства");
-            System.out.println("5. Вывести областные центры");
-            System.out.println("6. Вывести средний возраст жителей");
-            System.out.println("7. Вывести жителей с именем, состоящим из N букв");
-            System.out.println("8. Вывести список жителей, у которых имя начинается с буквы m");
-            System.out.println("9. Выйти из программы");
-            System.out.println("Введите номер команды: ");
-
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println(country.toString());
-                    break;
-                case 2:
-                    System.out.println("Столица: " + country.getCapital().getName());
-                    break;
-
-                case 3:
-                    System.out.println("Количество областей: " + country.getRegions().size());
-                    break;
-                case 4:
-                    double totalArea = 0;
-                    for (Region region : country.getRegions()) {
-                        totalArea += region.getArea();
-                    }
-                    System.out.println("Площадь государства: " + totalArea);
-                    break;
-                case 5:
-                    System.out.println("Областные центры:");
-                    for (Region region : country.getRegions()) {
-                        System.out.println("Область: " + region.getName() + ", Областной центр: " + region.getCenter());
-                    }
-                    break;
-                case 6:
-                    int totalAge = 0;
-                    int citizenCount = country.getCitizens().size();
-                    for (Citizen citizen : country.getCitizens()) {
-                        totalAge += citizen.getAge();
-                    }
-                    double averageAge = totalAge / (double) citizenCount;
-                    System.out.println("Средний возраст жителей: " + averageAge);
-                    break;
-                case 7:
-                    System.out.println("Введите количество букв в имени:");
-                    int letterCount = scanner.nextInt();
-                    System.out.println("Жители с именем, состоящим из " + letterCount + " букв:");
-                    for (Citizen citizen : country.getCitizens()) {
-                        if (citizen.getFirstName().length() == letterCount) {
-                            System.out.println(citizen);
-                        }
-                    }
-                    break;
-                case 8:
-                    System.out.println("Введите первую букву:");
-                    char startingLetter = scanner.next().charAt(0);
-                    country.printCitizensWithLetter(startingLetter);
-                    break;
-                case 9:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Некорректный выбор. Попробуйте снова.");
-            }
-        }
+        MainView view = new MainView();
+        MainController controller = new MainController(view, country);
+        controller.Run();
     }
 }
